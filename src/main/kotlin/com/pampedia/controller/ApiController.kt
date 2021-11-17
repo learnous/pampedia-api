3e3abea35a5f47199544ba5349536bcd28e06b5b
Learnous <sin_lhy@naver.com>
2021-11-17 16:17:16 +0900

    카카오-판타지 TOP50 크롤링-> div 내부 "일간 판타지 TOP"감응

--- src/main/kotlin/com/pampedia/controller/ApiController.kt
+++ src/main/kotlin/com/pampedia/controller/ApiController.kt
@@ -31,14 +31,19 @@ class ApiController {
         try {
             //get page (= 브라우저에서 url을 주소창에 넣은 후 request 한 것과 같다)
             driver[base_url]
-            val we = driver.findElementsByCssSelector(".css-1fyjypm")
-            println(we.get(2).text)
-            val tempArg = we.get(2).findElement(By.cssSelector("a div"))
-            driver.executeScript("arguments[0].click();", tempArg)
+            val we = driver.findElementsByCssSelector("div.css-1fyjypm")
+            for(idx in we){
+                if(idx.text.contains("일간 판타지 TOP")){
+                   val tempArg = idx.findElement(By.cssSelector("a div"))
+                    println(tempArg.text)
+                    driver.executeScript("arguments[0].click();", tempArg)
+                    break
+                }
+            }
 //            returnStr = tempArg.text
 //            println(driver.pageSource)
 //            returnStr = driver.currentUrl
-            Thread.sleep(2000)
+            Thread.sleep(3000)
             driver.executeScript("window.scrollTo(0, document.body.scrollHeight)")
             Thread.sleep(1000) // 50위까지 가져옴
 
--- src/test/kotlin/com/pampedia/ApplicationTests.kt
+++ src/test/kotlin/com/pampedia/ApplicationTests.kt
@@ -1,4 +1,4 @@
-package com.pampedia.demo
+package com.pampedia
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
